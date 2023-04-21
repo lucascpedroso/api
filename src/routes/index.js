d0d54998e59894b55366bf647599c3bbd29b7185
Lucas Pedroso <lucas.scpedroso@gmail.com>
2023-04-21 19:05:12 +0100

    added Notes controller and Notes routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,10 @@
 const { Router } = require("express")
 
 const userRoutes = require("./users.routes")
+const notesRoutes = require("./notes.routes")
 
 const routes = Router()
-
 routes.use("/users", userRoutes)
+routes.use("/notes", notesRoutes)
 
 module.exports = routes
