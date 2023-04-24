514b1d23e2312b04e6affb502a7bf9bf8576080f
Lucas Pedroso <lucas.scpedroso@gmail.com>
2023-04-24 18:42:05 +0100

    added delete and list notes functions

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -46,6 +46,22 @@ class NotesCrontoller {
             links
         })
     }
+
+    async delete(request, response) {
+        const { id } = request.params
+
+        await knex("notes").where({ id }).delete()
+
+        return response.json()
+    }
+
+    async index(request, response) {
+        const { user_id } = request.query
+
+        const notes = await knex("notes").where({ user_id }).orderBy("title")
+        
+        return response.json(notes)
+    }
 }
 
 module.exports = NotesCrontoller
