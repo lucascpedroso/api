1639da8a6c1df0cd513a15c3f38a5794b843ca18
Lucas Pedroso <lucas.scpedroso@gmail.com>
2023-04-25 19:30:48 +0100

    added tags controller and route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 const { Router } = require("express")
 
-const userRoutes = require("./users.routes")
-const notesRoutes = require("./notes.routes")
+const userRouter = require("./users.routes")
+const notesRouter = require("./notes.routes")
+const tagsRouter = require("./tags.routes")
 
 const routes = Router()
-routes.use("/users", userRoutes)
-routes.use("/notes", notesRoutes)
+routes.use("/users", userRouter)
+routes.use("/notes", notesRouter)
+routes.use("/tags", tagsRouter)
 
 module.exports = routes
