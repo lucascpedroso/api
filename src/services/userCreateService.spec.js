f0b111634b617e7f1373be27f0332f09c1c6e5ef
Lucas Pedroso <lucas.scpedroso@gmail.com>
2023-10-18 11:32:03 +0100

    Update JEST TEST

--- src/services/userCreateService.spec.js
+++ src/services/userCreateService.spec.js
@@ -1,18 +1,42 @@
 const UserCreateService = require("./UserCreateService")
-const UserRepositoryInMemory = require("../repositories/UserRepositoryInMemory")
+const UserRepositoryInMemory = require("../repositories/UserRepositoryInMemory");
+const AppError = require("../utils/AppError");
 
-// This function you have to include what you expect and the function that will run the test
+describe("UserCreateService", () => {
+    let userRepositoryInMemory = null
+    let userCreateService = null
 
-it("User should be created", async () => {
-    const user = {
-        name:  "User Test",
-        email: "user@test.com",
-        password: "123"
-    }
+    beforeEach(() => {
+        userRepositoryInMemory = new UserRepositoryInMemory()
+        userCreateService = new UserCreateService(userRepositoryInMemory)
+    })
+    // This function you have to include what you expect and the function that will run the test
+    it("User should be created", async () => {
+        const user = {
+            name:  "User Test",
+            email: "user@test.com",
+            password: "123"
+        }
 
-    const userRepositoryInMemory = new UserRepositoryInMemory()
-    const userCreateService = new UserCreateService(userRepositoryInMemory)
-    const userCreated = await userCreateService.execute(user)
+        const userCreated = await userCreateService.execute(user)
 
-    expect(userCreated).toHaveProperty("id")
-});
+        expect(userCreated).toHaveProperty("id")
+    });
+
+    it("User should not be created with an existing email", async () => {
+        const user1 = {
+            name:  "User1 Test",
+            email: "user@test.com",
+            password: "123"
+        }
+
+        const user2 = {
+            name:  "User2 Test",
+            email: "user@test.com",
+            password: "123"
+        }
+
+        await userCreateService.execute(user1)
+        await expect(userCreateService.execute(user2)).rejects.toEqual( new AppError("Email already in use!"))
+    })
+})
